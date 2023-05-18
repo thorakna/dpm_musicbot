e6380050e40d405d3bf1d643c5ecc58cec8355e3
Thorakna <arbedeci@gmail.com>
2023-05-18 13:31:30 +0300

    Initial commit for Evobot update

--- utils/config.ts
+++ utils/config.ts
@@ -8,12 +8,12 @@ try {
 } catch (error) {
   config = {
     TOKEN: process.env.TOKEN || "",
-    PREFIX: process.env.PREFIX || "!",
+    PREFIX: process.env.PREFIX || "/",
     MAX_PLAYLIST_SIZE: parseInt(process.env.MAX_PLAYLIST_SIZE!) || 10,
     PRUNING: process.env.PRUNING === "true" ? true : false,
     STAY_TIME: parseInt(process.env.STAY_TIME!) || 30,
     DEFAULT_VOLUME: parseInt(process.env.DEFAULT_VOLUME!) || 100,
-    LOCALE: process.env.LOCALE || "en"
+    LOCALE: process.env.LOCALE || "tr"
   };
 }
 
